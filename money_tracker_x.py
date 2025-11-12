f68b8628f884d45f1bb0df83890732700e2f7144
theCrimsonViper <pahyuhn.cinco@gmail.com>
2025-11-12 12:17:45 +0800

    created money_tracker_x.py
    setting up the modules
    
    and a directory for saving the .json file of records.

---
+++ money_tracker_x.py
@@ -0,0 +1,18 @@
+# money_tracker_x
+# keep track of all your household expenses
+
+# we will need the datetime module for date objects
+# sys module so we can direct the .json file containing the record
+# of coursse, we import the json module too
+# we will also import csv for exporting the records as csv
+# uuid module for creating unique entry IDs for each item recorded
+# lastly, pathlib module's Path for saving the file in your local directory
+
+from datetime import date, time, datetime
+from pathlib import Path
+import sys
+import uuid
+import json
+import csv
+
+DATE_FILE = Path.home() / "expenses_ledger"
